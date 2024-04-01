94be8080dcf7baafb394e1ecce4722172b797bfe
Ainur Rofiq (23090058) <149692250+Ainurrofiq575@users.noreply.github.com>
2024-04-01 22:16:20 +0700

    Update manajement_tugas.py

--- manajement_tugas.py
+++ manajement_tugas.py
@@ -1,11 +1,13 @@
 tasks = []
 while True:
+    
     print("\nDaftar Tugas:")
     for index, task in enumerate(tasks):
         print(f"{index + 1}. {task}")
     choice = input("\nTambahkan tugas baru (tekan 'oke' untuk keluar): ")
     if choice.lower() == 'oke':
         break
+        
     else:
         tasks.append(choice)
 print("\nTerima kasih! Daftar tugas Anda:")
